fac6bf0380529b724c4f00fbefd5b2ae0282444a
jisoo <doh_0112@naver.com>
2024-01-20 20:32:41 +0900

    feat: 타임아웃 에러 발생했을 경우 핸들링 추가

--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -1,34 +1,33 @@
 import MobileOrderLayout from "@/components/layout/MobileOrderLayout";
-import Header from "@/components/shared/Header";
 import React, { useEffect } from "react";
-import { ItemsType, useFetchAllItems } from "../../../hooks/order/useFetchItems";
-import Image from "next/image";
-import Item from "@/components/order/Item";
+import { useFetchAllItems } from "../../../hooks/order/useFetchItems";
 import ItemList from "@/components/order/ItemList";
+import { useRouter } from "next/router";
 
 const OrderPage = () => {
+  const router = useRouter();
   const { loadAllItems, isItemsLoading, error, response } = useFetchAllItems();
+
   useEffect(() => {
     loadAllItems();
   }, []);
-
-  useEffect(() => {
-    if (response) {
-      console.log(response);
-    }
-  }, [response]);
 
   // 로딩 중일 경우
   if (isItemsLoading) {
     return (
       <MobileOrderLayout>
         <div className="flex flex-col justify-center items-center h-full">
-          {/* <Image src={"/gif/loadingIcon.gif"} width={50} height={50} alt="로딩 중" /> */}
           <p>목록을</p>
           <p>불러오고 있습니다.</p>
         </div>
       </MobileOrderLayout>
     );
+  }
+
+  // 타임아웃 에러 발생했을 경우
+  if (error && error.code === "ECONNABORTED") {
+    alert("요청 시간 초과로 홈페이지로 이동합니다.");
+    router.push("/");
   }
 
   return (
